b4e7f1e6915fc34537723c091c947d0d0308dee9
Daniel Minshew <ofenixculpa@gmail.com>
2015-10-07 14:42:37 -0500

    skeletal structure based on first mock-up

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// Initialize collapse button
+ // $(".button-collapse").sideNav();
+ // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+ //$('.collapsible').collapsible();
+
+ // $('.button-collapse').sideNav({
+ //     menuWidth: 300, // Default is 240
+ //     edge: 'right', // Choose the horizontal origin
+ //     closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+ //   }
+ // );
